ad6e42380a88458248755421bd999e9f729fad23
ngocsangyem <nnsang24@gmail.com>
2019-07-15 14:39:54 +0700

    add article structure

--- src/app/article/article-routing.module.ts
+++ src/app/article/article-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { ArticlePreviewComponent } from './pages/article-preview/article-preview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: '',
+		redirectTo: 'articles',
+		component: ArticlesComponent
+	},
+	{
+		path: ':id',
+		component: ArticlePreviewComponent
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forChild(routes)],
+	exports: [RouterModule]
 })
-export class ArticleRoutingModule { }
+export class ArticleRoutingModule {}
--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { ArticleRoutingModule } from './article-routing.module';
 import {SharedModule} from '../shared/shared.module';
+import { ArticlePreviewComponent } from './pages/article-preview/article-preview.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { ArticleEditComponent } from './pages/article-edit/article-edit.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ArticlePreviewComponent, ArticlesComponent, ArticleEditComponent],
   imports: [
     ArticleRoutingModule,
     SharedModule
